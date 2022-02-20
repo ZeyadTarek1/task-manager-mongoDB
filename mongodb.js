6556ad809a4d0a1bb12f5b052c528268d29c5f46
ZeyadTarek1 <zeyadtaziz@gmail.com>
2022-02-20 16:03:16 +0200

    insertOne insertMany methods

--- mongodb.js
+++ mongodb.js
@@ -1,3 +1,8 @@
+// Not Only Structered Query Language
+// *** Vocab ***
+// Table = Collection = list of entries
+// Row = Document = individual entry
+// Column = Field
 // CRUD = create read update delete
 
 const mongodb = require("mongodb");
@@ -6,6 +11,7 @@ const MongoClient = mongodb.MongoClient;
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
+// connect to server
 MongoClient.connect(
     connectionURL,
     { useNewUrlParser: true },
@@ -14,6 +20,45 @@ MongoClient.connect(
             return console.log("Unable to connect to database");
         }
 
+        //create a database reference
         const db = client.db(databaseName);
+
+        //insert new document in a collection called Users
+        db.collection("Users").insertOne(
+            {
+                name: "Ziad",
+                age: 26,
+            },
+            (error, result) => {
+                if (error) {
+                    return console.log("Unable to insert");
+                }
+                console.log(result.insertedId);
+            }
+        );
+
+        //insert multiple documents in a new collection called Tasks
+        db.collection("Tasks").insertMany(
+            [
+                {
+                    description: "test",
+                    completed: true,
+                },
+                {
+                    description: "test2",
+                    completed: true,
+                },
+                {
+                    description: "test3",
+                    completed: false,
+                },
+            ],
+            (error, result) => {
+                if (error) {
+                    ("Unable to insert data");
+                }
+                console.log(result.insertedIds);
+            }
+        );
     }
 );
