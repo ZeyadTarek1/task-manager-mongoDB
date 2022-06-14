c03c1d1f5ed3b295a6a974b04a887b4265ff3893
Zeyad <zeyadtaziz@gmail.com>
2022-06-14 12:28:11 +0200

    basic user login

--- src/models/user.js
+++ src/models/user.js
@@ -12,6 +12,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -43,15 +44,27 @@ const userSchema = new mongoose.Schema({
     },
 });
 
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new Error("Unable to login");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        throw new Error("Unable to login");
+    }
+
+    return user;
+};
+
+// password hashing before saving
 userSchema.pre("save", async function (next) {
     // user being saved can be accessed by this
     const user = this;
-    console.log(user.password);
     if (user.isModified("password")) {
         user.password = await bcrypt.hash(user.password, 8);
     }
-
-    console.log("just before saving");
 
     next(); //call this function to exit out of userSchema.pre
 });
